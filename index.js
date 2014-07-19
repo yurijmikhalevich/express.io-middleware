d7e986686a4746d4efbdbff6f2b0303786bc039c
Yurij Mikhalevich <0@39.yt>
2014-07-19 18:21:27 +0400

    Updated repo url

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 /**
- * Copyright (C) 2013 Cat Limited, Yurij Mikhalevich
- * @author: Yurij Mikhalevich <0@39.yt>
+ * @license GPLv3
+ * @author 0@39.yt (Yurij Mikhalevich)
  */
-
 var _ = require('underscore');
 
 /**
- * Extends app.io objects, adding middleware functionality and event and namespace properties, which stores event name
- * and namespace (before ':' part) respectively.
- *
+ * Extends app.io objects, adding middleware functionality and event and
+ * namespace properties, which stores event name and namespace (before ':' part)
+ * respectively.
  * @param {Object} app express.io app object
  */
 module.exports = function(app) {
   /**
    * Overridden app.io.route method, which should use our registerRoute.
-   *
    * @param {String} route Signal name
-   * @param {Function|Object} next Signal handler or request object (according to options.trigger), as request object
-   * used for internal express.io purposes
-   * @param {Object} [options] Additional parameters, options.trigger makes sense and used for internal express.io
+   * @param {Function|Object} next Signal handler or request object (according
+   * to options.trigger), as request object used for internal express.io
    * purposes
+   * @param {Object} [options] Additional parameters, options.trigger makes
+   * sense and used for internal express.io purposes
    * @returns {Object|Array}
    */
   app.io.route = function(route, next, options) {
@@ -40,7 +39,8 @@ module.exports = function(app) {
         if (!next.hasOwnProperty(key)) {
           continue;
         }
-        _results.push(registerRoute(this.router, this.middleware, "" + route + ":" + key, next[key]));
+        _results.push(registerRoute(this.router, this.middleware, "" + route +
+            ":" + key, next[key]));
       }
       return _results;
     }
@@ -48,8 +48,8 @@ module.exports = function(app) {
 
   /**
    * Registers middleware in system.
-   *
-   * @param {RegExp|Function} regexp RegExp, which specifies events, for which middleware should be applied
+   * @param {RegExp|Function} regexp RegExp, which specifies events, for which
+   * middleware should be applied
    * or middleware
    * @param {Function} [middleware] Middleware function
    * @returns {Array}
@@ -65,7 +65,6 @@ module.exports = function(app) {
 
 /**
  * Registers new route in the route subsystem
- *
  * @param {Object} router Router object, expects app.io.router
  * @param {Array} middlewares Middlwares array, expects app.io.middleware
  * @param {String} route Signal name
@@ -77,12 +76,15 @@ function registerRoute(router, middlewares, route, func) {
   if (previousFunc) { // if another handler exists, adding new in chain
     return router[route] = createRouteFunction(previousFunc, func);
   } else { // if there is no another handler
-    middlewares.forEach(function (middleware) { // compile all middlewares in chain
+    middlewares.forEach(function (middleware) {
+    // compile all middlewares in chain
       if (middleware[0].test(route)) {
         if (previousFunc) {
-          previousFunc = router[route] = createRouteFunction(previousFunc, middleware[1]);
+          previousFunc = router[route] =
+              createRouteFunction(previousFunc, middleware[1]);
         } else {
-          previousFunc = router[route] = createRouteFunction(middleware[1], route);
+          previousFunc = router[route] =
+              createRouteFunction(middleware[1], route);
         }
       }
     });
@@ -96,8 +98,8 @@ function registerRoute(router, middlewares, route, func) {
 
 /**
  * Creates chain route function
- *
- * @param {Function} previousFunc Previous function in chain or new function in chain, according to second argument
+ * @param {Function} previousFunc Previous function in chain or new function in
+ * chain, according to second argument
  * @param {Function|String} func New function in chain or Signal name
  * @returns {Function}
  */
